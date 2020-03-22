ba776dfd26ff4a7e1d37bfff86c89deddcc2c403
bres <geobres@gmail.com>
2020-03-22 22:07:45 +0200

    finalize the crypto site

--- mysite/crypto/views.py
+++ mysite/crypto/views.py
@@ -13,3 +13,15 @@ def home(request):
     api_request =requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api =json.loads(api_request.content)
     return render(request,'home.html',{'api':api,'price':price})
+
+def prices(request):
+    if request.method == 'POST':
+        import requests
+        import json
+        quote = request.POST['quote']
+        quote= quote.upper()
+        crypto_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + quote + "&tsyms=EUR")
+        crypto = json.loads(crypto_request.content)
+        return render(request ,'prices.html',{'quote':quote ,'crypto':crypto})
+    else:
+        return render(request,'prices.html',{})
